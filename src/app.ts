0fbbcbdcd9e259767b47f78e6cfeb77a88f57409
Turjo-abhishek <turjoabhishek@gmail.com>
2023-06-04 14:20:28 +0600

    setup complete husky eslint prettier lint staged

--- src/app.ts
+++ src/app.ts
@@ -11,5 +11,10 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req: Request, res: Response) => {
   res.send('working properly')
 })
+app.get('/test', (req: Request, res: Response) => {
+  res.send('working properly testing')
+})
+
+
 
 export default app
